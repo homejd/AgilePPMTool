ef6ebd31957a9736e95f0222b470b71e0f1e5d59
homejd <homew09@gmail.com>
2021-07-01 02:15:36 +0200

    first commit

---
+++ ppmtool/src/main/java/io/jdtec/pptool/PptoolApplication.java
@@ -0,0 +1,13 @@
+package io.jdtec.pptool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PptoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PptoolApplication.class, args);
+	}
+
+}
---
+++ ppmtool/src/test/java/io/jdtec/pptool/PptoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jdtec.pptool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PptoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
